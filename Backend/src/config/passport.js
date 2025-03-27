2e23ccabf33a7773f523f00454aee80439257f4b
Tahmidul Islam Omi <tahmidulislamomi09@gmail.com>
2025-03-27 19:45:26 +0600

    Trying to fix the URL Issue

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -25,7 +25,9 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/api/v1/auth/google/callback',
+            callbackURL: process.env.NODE_ENV === 'production' 
+                ? 'https://voyagebot.vercel.app/api/v1/auth/google/callback'
+                : '/api/v1/auth/google/callback',
             scope: ['profile', 'email']
         },
         async (accessToken, refreshToken, profile, done) => {
